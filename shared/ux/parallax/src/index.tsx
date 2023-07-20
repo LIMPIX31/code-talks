0fce0570cb3137e6e09c4ff5d96117f6e20c51df
Danil Karpenko <limpix31@gmail.com>
2023-07-20 11:45:28 +0200

    fix: parallax window object use

--- shared/ux/parallax/src/index.tsx
+++ shared/ux/parallax/src/index.tsx
@@ -8,16 +8,14 @@ export function makeVerticalParallax<C extends keyof typeof motion>(
 	component?: C,
 ): FC<ComponentProps<C>> {
 	return (props) => {
+		const viewportHeight = typeof window !== 'undefined' ? window.innerHeight : 1
+
 		const ref = useRef<HTMLDivElement>(null!)
 
 		const { scrollY } = useScroll()
 
 		const deltas = Object.entries(distance).reduce<any>((a, [key, { in: iin, out }]) => {
-			a[key] = useTransform(
-				scrollY,
-				iin ?? [page * window.innerHeight, page * window.innerHeight + window.innerHeight],
-				out,
-			)
+			a[key] = useTransform(scrollY, iin ?? [page * viewportHeight, page * viewportHeight + viewportHeight], out)
 			return a
 		}, {})
 
