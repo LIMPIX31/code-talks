ad02d4b8f75cdba6cc29a3bccf2c61bdbd30011c
Danil Karpenko <limpix31@gmail.com>
2023-07-20 13:25:48 +0200

    feat: guild join route

---
+++ app/entrypoint/app/api/discord/join/route.ts
@@ -0,0 +1,26 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export default async function POST(req: NextRequest) {
+	const { token, user } = await req.json()
+
+	if (!token || !user) {
+		return new Response(null, { status: 401 })
+	}
+
+	const { status } = await fetch(`https://discord.com/api/v10/guilds/${process.env.GUILD_ID}/members/${USER}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			access_token: token,
+		}),
+		headers: {
+			Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+		},
+	})
+
+	if (status !== 201) {
+		return new Response(null, { status: 403 })
+	}
+
+	return NextResponse.json({ ok: true })
+}
