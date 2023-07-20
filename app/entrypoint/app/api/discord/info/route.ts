66ee7eec2239104e716bc4a61c9f88eac4fe7bd2
Danil Karpenko <limpix31@gmail.com>
2023-07-20 08:31:44 +0200

    feat: discord guild info api

---
+++ app/entrypoint/app/api/discord/info/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+	console.log(process.env.DISCORD_BOT_TOKEN)
+
+	const info = await fetch(`https://discord.com/api/v10/guilds/${process.env.GUILD_ID}`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+		},
+	}).then((res) => res.json())
+
+	return NextResponse.json(info)
+}
