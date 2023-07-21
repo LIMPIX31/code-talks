5e96a90f575f4eebdfd4dd1165229f9916a4c1b8
Danil Karpenko <limpix31@gmail.com>
2023-07-21 08:49:45 +0200

    feat: improve parallax

--- shared/ux/parallax/src/index.tsx
+++ shared/ux/parallax/src/index.tsx
@@ -4,18 +4,19 @@ import { ComponentProps, FC, RefObject, useMemo } from 'react'
 import { useScroll, useTransform, motion } from 'framer-motion'
 
 export interface ScrollyProps<C extends keyof typeof motion> {
-	distance: { [k: string]: { in?: number[]; out: number[] } }
+	distance: { [k: string]: [number, number, number] }
 	options: {
 		component?: C
 		bindTo?: string | 'root'
 		offset?: [number | 'self', number | 'self'] | number
 		relativeTo?: RefObject<HTMLElement>
+		speed?: number
 	}
 }
 
 const Scrolly = <C extends keyof typeof motion>({
 	distance,
-	options: { component, bindTo, offset = [0, 0], relativeTo },
+	options: { component, bindTo, offset = [0, 0], relativeTo, speed = 1 },
 	...props
 }: ComponentProps<C> & ScrollyProps<C>) => {
 	const viewportHeight = typeof window !== 'undefined' ? window.innerHeight : 1
@@ -24,13 +25,13 @@ const Scrolly = <C extends keyof typeof motion>({
 
 	const range = useMemo(() => {
 		if (typeof document === 'undefined') {
-			return [0, 1]
+			return [0, 0, 1]
 		}
 
 		const anchor = bindTo === 'root' ? document.body : document.querySelector(`[data-parallax-id="${bindTo}"]`)
 
 		if (!anchor) {
-			return [0, viewportHeight]
+			return [0, 0, viewportHeight]
 		}
 
 		const { top: topV, bottom: bottomV, height } = anchor.getBoundingClientRect()
@@ -50,12 +51,12 @@ const Scrolly = <C extends keyof typeof motion>({
 			offsetY = -height
 		}
 
-		return [top + offsetX, bottom + offsetY]
+		return [(top + offsetX - height) * speed, top + offsetX, (bottom + offsetY) * speed]
 	}, [bindTo, offset, relativeTo, viewportHeight])
 
-	const deltas = Object.entries(distance).reduce<any>((a, [key, { in: iin, out }]) => {
+	const deltas = Object.entries(distance).reduce<any>((a, [key, out]) => {
 		// eslint-disable-next-line react-hooks/rules-of-hooks
-		a[key] = useTransform(scrollY, iin ?? range, out)
+		a[key] = useTransform(scrollY, range, out)
 		return a
 	}, {})
 
@@ -66,12 +67,13 @@ const Scrolly = <C extends keyof typeof motion>({
 
 export const scrolly =
 	<C extends keyof typeof motion>(
-		distance: { [k: string]: { in?: number[]; out: number[] } },
+		distance: { [k: string]: [number, number, number] },
 		options: {
 			component?: C
 			bindTo?: string | 'root'
 			offset?: [number | 'self', number | 'self'] | number
 			relativeTo?: RefObject<HTMLElement>
+			speed?: number
 		},
 	): FC<ComponentProps<C>> =>
 	(props) => <Scrolly distance={distance} options={options} {...props} />
