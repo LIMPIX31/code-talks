ff108d9409bffb2abf27cc12aba08245e3a58529
Danil Karpenko <limpix31@gmail.com>
2023-07-23 12:00:45 +0200

    chore: split hooks

--- shared/supabase/client/src/index.ts
+++ shared/supabase/client/src/index.ts
@@ -1,5 +1,2 @@
-import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-
-export const useClient = createClientComponentClient
-
-export const useServerClient = createServerComponentClient
+export * from './client'
+export * from './server-client'
