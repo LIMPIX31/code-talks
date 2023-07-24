2ff554016d0f0f37792ec09f9d6e254682bf622f
Danil Karpenko <limpix31@gmail.com>
2023-07-24 17:27:01 +0200

    feat: metadata and robots.txt

--- app/entrypoint/app/thanks/page.tsx
+++ app/entrypoint/app/thanks/page.tsx
@@ -1,6 +1,11 @@
 import dynamic from 'next/dynamic'
+import type { Metadata } from 'next'
 
 const ThanksPage = dynamic(() => import('@page/thanks'))
+
+export const metadata: Metadata = {
+	title: 'Thanks',
+}
 
 export default function Page() {
 	return <ThanksPage />
